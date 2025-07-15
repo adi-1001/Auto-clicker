71e494a84f507c25867eb4bdf9c34d7f32f9df32
Aditya Anshu <sayhellotomr.aditya@gmail.com>
2025-07-15 12:16:38 +0530

    cordinate script created

---
+++ CoordinateChecker.py
@@ -0,0 +1,10 @@
+import pyautogui
+import time
+
+print("Move your mouse to the target location...")
+time.sleep(5)
+print("Your position is:", pyautogui.position())
+# point 1 Point(x=736, y=292) right click
+# point 2 Point(x=939, y=676) left click
+# point 3 Point(x=1034, y=825) left click
+# point 4 Point(x=1519, y=905) left click
